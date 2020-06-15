dd28125706b0b9fc1e119d857262e25b309b8eeb
Keith Gaughan <k@stereochro.me>
2020-06-16 00:13:58 +0100

    Just load from the command line

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module mercury
 
-go 1.14
+go 1.13
 
-require github.com/urfave/cli/v2 v2.2.0
+require github.com/BurntSushi/toml v0.3.1
--- main.go
+++ main.go
@@ -1,103 +1,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path"
 	"time"
 
-	"github.com/urfave/cli/v2"
+	"github.com/BurntSushi/toml"
 )
 
 // Requirements:
 //
 //  - Load a configuration file
 
+type duration struct {
+	time.Duration
+}
+
+func (d *duration) UnmarshalText(text []byte) error {
+	var err error
+	d.Duration, err = time.ParseDuration(string(text))
+	return err
+}
+
+var configPath = flag.String("config", "./mercury.toml", "Path to configuration")
+
+type feed struct {
+	Name string
+	Feed string
+}
+
+type Config struct {
+	Name    string
+	URL     string `toml:url`
+	Owner   string
+	Email   string
+	Cache   string
+	Timeout duration
+	Theme   string
+	Output  string
+	Feed    []feed
+}
+
 func main() {
-	var config string
-	var name string
-	var baseUrl string
-	var ownerName string
-	var ownerEmail string
-	var cachePath string
-	var timeout time.Duration
-	var themePath string
-	var outputPath string
-	var itemsPerPage int
+	var config Config
 
-	app := &cli.App{
-		Name:  "mercury",
-		Usage: "A feed aggregator in the style of Planet",
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", path.Base(os.Args[0]))
 
-		Flags: []cli.Flag{
-			&cli.PathFlag{
-				Name:        "config",
-				Usage:       "Load configuration from `FILE`",
-				Value:       "./mercury.json",
-				Destination: &config,
-			},
-			&cli.StringFlag{
-				Name:        "name",
-				Value:       "My Planet!",
-				Usage:       "Your planet's `name`",
-				Destination: &name,
-			},
-			&cli.StringFlag{
-				Name:        "base-url",
-				Value:       "http://localhost/",
-				Usage:       "`URL` of the main page",
-				Destination: &baseUrl,
-			},
-			&cli.StringFlag{
-				Name:        "owner-name",
-				Usage:       "Your `name`",
-				Destination: &ownerName,
-			},
-			&cli.StringFlag{
-				Name:        "owner-email",
-				Usage:       "Your `email` address",
-				Destination: &ownerEmail,
-			},
-			&cli.PathFlag{
-				Name:        "cache",
-				Usage:       "Path to where cached feeds are stored",
-				Value:       "./cache",
-				Destination: &cachePath,
-			},
-			&cli.DurationFlag{
-				Name:        "timeout",
-				Usage:       "Number of `seconds` to wait on a given feed",
-				Value:       time.Second * 20,
-				Destination: &timeout,
-			},
-			&cli.PathFlag{
-				Name:        "theme",
-				Usage:       "Path to your theme's templates",
-				Value:       "./theme",
-				Destination: &themePath,
-			},
-			&cli.PathFlag{
-				Name:        "output",
-				Usage:       "Directory to write the site files",
-				Value:       "./output",
-				Destination: &outputPath,
-			},
-			&cli.IntFlag{
-				Name:        "items-per-page",
-				Value:       60,
-				Usage:       "How many items to put on each page",
-				Destination: &itemsPerPage,
-			},
-		},
-
-		Action: func(c *cli.Context) error {
-			fmt.Println("Hello, friend!")
-			return nil
-		},
+		flag.PrintDefaults()
 	}
 
-	err := app.Run(os.Args)
-	if err != nil {
+	flag.Parse()
+	if _, err := toml.DecodeFile(*configPath, &config); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("%+v", config)
 }
