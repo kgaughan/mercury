b83d2e48379d51099189abec5cb4fb93a587fa62
Keith Gaughan <k@stereochro.me>
2020-06-16 03:23:36 +0100

    Cache manifest marshalling

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mercury
 
 go 1.13
 
-require github.com/BurntSushi/toml v0.3.1
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/google/uuid v1.1.1
+)
--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path"
 	"time"
 
 	"github.com/BurntSushi/toml"
+	"github.com/google/uuid"
 )
-
-// Requirements:
-//
-//  - Load a configuration file
 
 type duration struct {
 	time.Duration
@@ -44,18 +43,75 @@ type Config struct {
 	Feed    []feed
 }
 
+func (c *Config) Load(path string) error {
+	_, err := toml.DecodeFile(path, c)
+	return err
+}
+
+type cacheItem struct {
+	UUID         string // Used to identify the cached feed
+	LastModified string // Used for conditional GET
+	ETag         string // Also used for conditional GET
+}
+
+type manifest map[string]*cacheItem
+
+func (m *manifest) Load(path string) error {
+	if file, err := ioutil.ReadFile(path); err != nil {
+		return err
+	} else if err := json.Unmarshal(file, m); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *manifest) Save(path string) error {
+	if file, err := json.Marshal(m); err == nil {
+		return ioutil.WriteFile(path, file, 0600)
+	} else {
+		return err
+	}
+}
+
 func main() {
 	var config Config
-
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", path.Base(os.Args[0]))
-
 		flag.PrintDefaults()
 	}
-
 	flag.Parse()
-	if _, err := toml.DecodeFile(*configPath, &config); err != nil {
+
+	if err := config.Load(*configPath); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%+v", config)
+
+	if fileInfo, err := os.Stat(config.Cache); os.IsNotExist(err) {
+		log.Fatal(err)
+	} else if !fileInfo.IsDir() {
+		log.Fatalf("%s must be a directory\n")
+	}
+
+	manifestPath := path.Join(config.Cache, "manifest.json")
+	cachedManifest := make(manifest)
+	if err := cachedManifest.Load(manifestPath); err != nil {
+		log.Fatal(err)
+	}
+
+	// Populate the manifest with the contents of the config file
+	manifest := make(manifest)
+	for _, feed := range config.Feed {
+		if item, ok := cachedManifest[feed.Feed]; ok {
+			// Copy over the extant cache entry
+			manifest[feed.Feed] = item
+		} else {
+			// New feed: create a new record
+			manifest[feed.Feed] = &cacheItem{
+				UUID: uuid.New().String(),
+			}
+		}
+	}
+
+	if err := manifest.Save(manifestPath); err != nil {
+		log.Fatal(err)
+	}
 }
