5f91aaa736e92aff12068b2a05dbfd0fbf6949a9
Keith Gaughan <keith@gaughan.ie>
2022-12-01 02:46:01 +0000

    Tidy opml2config.py (#18)
    Modernise the code a tad to use the walrus operator and fix the usage.

--- tools/opml2config.py
+++ tools/opml2config.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import sys
 from os import path
+import sys
+from typing import Iterator, Tuple
 from xml.dom import minidom
 
 ESCAPES = {
@@ -15,33 +16,27 @@ ESCAPES = {
 }
 
 
+def extract_feeds(doc: minidom.Document) -> Iterator[Tuple[str, str]]:
+    for node in doc.getElementsByTagName("outline"):
+        if node.getAttribute("type") == "rss":
+            if feed := node.getAttribute("xmlUrl"):
+                yield (feed, node.getAttribute("text"))
+
+
 def main():
     if len(sys.argv) == 2 and sys.argv[1] == "-h":
-        print("usage:", path.basename(sys.argv[0]), "<opml>", file=sys.stderr)
+        print("usage:", path.basename(sys.argv[0]), "<opml>*", file=sys.stderr)
         sys.exit(0)
 
     escapes = str.maketrans(ESCAPES)
-
-    first = True
     for filename in sys.argv[1:]:
         if not path.exists(filename):
-            print("error: no such file:", filename, file=sys.stderr)
-            sys.exit(1)
-        doc = minidom.parse(filename)
-        for node in doc.getElementsByTagName("outline"):
-            node_type = node.getAttribute("type")
-            if node_type != "rss":
-                continue
-            if first:
-                first = False
-            else:
-                print()
-            name = node.getAttribute("text").translate(escapes)
-            feed = node.getAttribute("xmlUrl").translate(escapes)
-            if feed:
-                print("[[feed]]")
-                print(f'name = "{name}"')
-                print(f'feed = "{feed}"')
+            sys.exit(f"error: no such file: {filename}")
+        for feed, name in extract_feeds(minidom.parse(filename)):
+            print("[[feed]]")
+            print(f'name = "{name.translate(escapes)}"')
+            print(f'feed = "{feed.translate(escapes)}"')
+            print()
 
 
 if __name__ == "__main__":
