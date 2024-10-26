5f6dfbf1ec46f0329deb23ae034495cb8cfed532
Keith Gaughan <keith@gaughan.ie>
2024-10-26 03:44:22 +0100

    Add cache directory tagging support (#114)
    See: https://bford.info/cachedir/
    
    This is a small tweak to make sure that the cache directory can be
    identified as such.

--- internal/utils/paths.go
+++ internal/utils/paths.go
@@ -1,9 +1,13 @@
 package utils
 
 import (
+	"bytes"
 	"log"
 	"os"
+	"path/filepath"
 )
+
+const cacheTagMarker = "Signature: 8a477f597d28d172789f06886806bc55\n"
 
 func EnsureDir(path string) {
 	if fileInfo, err := os.Stat(path); os.IsNotExist(err) {
@@ -12,5 +16,29 @@ func EnsureDir(path string) {
 		}
 	} else if !fileInfo.IsDir() {
 		log.Fatalf("%s must be a directory\n", path)
+	}
+}
+
+func writeCacheTag(path string) error {
+	return os.WriteFile(path, []byte(cacheTagMarker), 0o600)
+}
+
+func EnsureCache(path string) {
+	EnsureDir(path)
+	cacheTag := filepath.Join(path, "CACHE.TAG")
+	if _, err := os.Stat(cacheTag); os.IsNotExist(err) {
+		if err = writeCacheTag(cacheTag); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		contents, err := os.ReadFile(cacheTag)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if !bytes.HasPrefix(contents, []byte(cacheTagMarker)) {
+			if err = writeCacheTag(cacheTag); err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
