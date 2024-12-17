b5d1a4ec11700c5fa88692473b93407a427730f5
Keith Gaughan <k@stereochro.me>
2024-12-17 21:59:16 +0000

    Fix alignment again

--- internal/config.go
+++ internal/config.go
@@ -18,22 +18,22 @@ const cpuLimit = 32 // Cap on the number of CPUs/cores to use
 
 // Config describes our configuration.
 type Config struct {
-	Name          string          `toml:"name"`
-	URL           string          `toml:"url"`
-	Owner         string          `toml:"owner"`
-	Email         string          `toml:"email"`
-	FeedID        string          `toml:"feed_id"`
-	Cache         string          `toml:"cache"`
-	Timeout       utils.Duration  `toml:"timeout"`
-	themePath     string          `toml:"theme"`
-	Theme         fs.FS           `toml:"-"`
-	Output        string          `toml:"output"`
-	Feeds         []manifest.Feed `toml:"feed"`
-	ItemsPerPage  int             `toml:"items"`
-	MaxPages      int             `toml:"max_pages"`
-	JobQueueDepth int             `toml:"job_queue_depth"`
-	Parallelism   int             `toml:"parallelism"`
-	Filters      map[string]filters.Filter `toml:"filter"`
+	Name          string                    `toml:"name"`
+	URL           string                    `toml:"url"`
+	Owner         string                    `toml:"owner"`
+	Email         string                    `toml:"email"`
+	FeedID        string                    `toml:"feed_id"`
+	Cache         string                    `toml:"cache"`
+	Timeout       utils.Duration            `toml:"timeout"`
+	themePath     string                    `toml:"theme"`
+	Theme         fs.FS                     `toml:"-"`
+	Output        string                    `toml:"output"`
+	Feeds         []manifest.Feed           `toml:"feed"`
+	ItemsPerPage  int                       `toml:"items"`
+	MaxPages      int                       `toml:"max_pages"`
+	JobQueueDepth int                       `toml:"job_queue_depth"`
+	Parallelism   int                       `toml:"parallelism"`
+	Filters       map[string]filters.Filter `toml:"filter"`
 }
 
 // Load loads our configuration file.
