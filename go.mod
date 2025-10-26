7e43242647a3f01f24609cde133ef38cba7a6d3e
Keith Gaughan <keith@gaughan.ie>
2025-10-26 21:47:21 +0000

    Try out testify by adding a test for excerpt (#166)

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/mmcdole/gofeed v1.3.0
 	github.com/pquerna/cachecontrol v0.2.0
 	github.com/spf13/pflag v1.0.9
+	github.com/stretchr/testify v1.11.1
 	golang.org/x/net v0.46.0
 )
 
@@ -22,6 +23,7 @@ require (
 	github.com/PuerkitoBio/goquery v1.10.3 // indirect
 	github.com/andybalholm/cascadia v1.3.3 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/huandu/xstrings v1.5.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -30,8 +32,10 @@ require (
 	github.com/mmcdole/goxpp v1.1.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/spf13/cast v1.9.2 // indirect
 	golang.org/x/crypto v0.43.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/templates/excerpt_test.go
+++ internal/templates/excerpt_test.go
@@ -1,15 +1,36 @@
 package templates
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestTruncateText(t *testing.T) {
-	if result, n := truncateText("this has multiple words", 10); result != "this has" || n != 0 {
-		t.Errorf("got %q with %d remaining", result, n)
-	}
-	if result, n := truncateText("this has multiple words", 2); result != "th" || n != 0 {
-		t.Errorf("got %q with %d remaining", result, n)
-	}
-	if result, n := truncateText("this is short", 20); result != "this is short" || n != 7 {
-		t.Errorf("got %q with %d remaining", result, n)
-	}
+	result, n := truncateText("this has multiple words", 10)
+	assert.Equal(t, "this has", result)
+	assert.Equal(t, 0, n)
+
+	result, n = truncateText("this has multiple words", 2)
+	assert.Equal(t, "th", result)
+	assert.Equal(t, 0, n)
+
+	result, n = truncateText("this is short", 20)
+	assert.Equal(t, "this is short", result)
+	assert.Equal(t, 7, n)
+}
+
+func TestExcerpt(t *testing.T) {
+	htmlInput := `<p>This is a <strong>test</strong> of the excerpt function. It should <em>correctly</em> handle <a href="#">HTML</a> tags and truncate the text appropriately.</p>`
+
+	result := excerpt(htmlInput, 5)
+	expected := "<p>This\u2026</p>"
+	assert.Equal(t, expected, result)
+
+	result = excerpt(htmlInput, 20)
+	expected = "<p>This is a <strong>test</strong> of the excerpt\u2026</p>"
+	assert.Equal(t, expected, result)
+
+	result = excerpt(htmlInput, 100)
+	assert.Equal(t, htmlInput, result)
 }
